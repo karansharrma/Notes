1570fc65b2cf6825790129a70d8e545b309e2bbe
Karan <karanbharadwaj394@gmail.com>
2025-05-10 12:22:30 +0530

    added cloudinary credentials to .env

--- cloudinary.js
+++ cloudinary.js
@@ -1,10 +1,11 @@
 // config/cloudinary.js
 const cloudinary = require("cloudinary").v2;
+require("dotenv").config()
 
 cloudinary.config({
-  cloud_name: "dvnjmf1dn",
-  api_key: "482329856273997",
-  api_secret: "fpJGCtFzMt812xQYfWzD99PulqQ",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 module.exports = cloudinary;
